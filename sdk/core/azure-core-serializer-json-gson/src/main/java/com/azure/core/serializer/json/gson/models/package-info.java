0482bee9fc9107434a210f572e0c72561e34f9eb
Marvin SSC <151462975+marvin-acme@users.noreply.github.com>
2025-04-21 13:31:07 +0200

    Delete sdk/core directory

--- sdk/core/azure-core-serializer-json-gson/src/main/java/com/azure/core/serializer/json/gson/models/package-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-/**
- * Package containing models that represent JSON using the Java class system.
- */
-package com.azure.core.serializer.json.gson.models;
