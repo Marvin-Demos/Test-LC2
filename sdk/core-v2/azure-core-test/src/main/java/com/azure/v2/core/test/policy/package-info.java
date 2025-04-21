729a8ba73166cf4454ac2b51a3c5dde0ef5efee1
Marvin SSC <151462975+marvin-acme@users.noreply.github.com>
2025-04-21 13:30:32 +0200

    Delete sdk/core-v2 directory

--- sdk/core-v2/azure-core-test/src/main/java/com/azure/v2/core/test/policy/package-info.java
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-/**
- * Package containing {@link io.clientcore.core.http.pipeline.HttpPipelinePolicy HttpPipelinePolicies} used to test
- * Azure client libraries.
- */
-package com.azure.v2.core.test.policy;
